a5f8469aea971c21e306ac070f3a6d50faf1c9e7
Max Wittmann <damxam@gmail.com>
2018-05-06 11:40:06 +1000

    Rewrite Lake to use new builder

--- src/main/scala/templatey/b/Definition.scala
+++ src/main/scala/templatey/b/Definition.scala
@@ -70,8 +70,6 @@ object Definition {
     ) extends Edge
 
     object Builders3 {
-
-
       def vertex(name: String): VertexBuilder = VertexBuilder(name, Seq.empty)
 
       case class VertexBuilder(name: String, versions: Seq[VertexVersion]) {
@@ -88,46 +86,29 @@ object Definition {
         def defn(name: String) = DefnBuilder(this, Some(name), Seq.empty, Seq.empty)
         def defn = DefnBuilder(this, None, Seq.empty, Seq.empty)
 
-//        def withDefn(version: VertexDefinition): CompleteVersionBuilder
         def withDefn(defn: VertexDefinition): CompleteVersionBuilder =
           CompleteVersionBuilder(parent, Seq(defn))
       }
 
       case class IncompleteVersionBuilder(parent: VertexBuilder) extends VersionBuilder {
-//        def defn(name: String) = DefnBuilder(this, Some(name), Seq.empty, Seq.empty)
-//        def defn = DefnBuilder(this, None, Seq.empty, Seq.empty)
-
-//        override def withDefn(defn: VertexDefinition): CompleteVersionBuilder =
-//          CompleteVersionBuilder(parent, Seq(defn))
-
         override def definitions: Seq[VertexDefinition] = Seq.empty
       }
 
-      case class CompleteVersionBuilder(parent: VertexBuilder, definitions: Seq[VertexDefinition]) extends VersionBuilder  {
-//        def defn(name: String) = DefnBuilder(this, Some(name), Seq.empty, Seq.empty)
-//        def defn = DefnBuilder(this, None, Seq.empty, Seq.empty)
-
-//        override def withDefn(defn: VertexDefinition): CompleteVersionBuilder =
-//          CompleteVersionBuilder(parent, Seq(defn))
-
-//        def version: VersionBuilder = {
-//          IncompleteVersionBuilder(parent.copy(versions = VertexVersion(definitions) +: parent.versions))
-//        }
-
-//        def done: VertexBuilder = parent.copy(versions = VertexVersion(definitions) +: parent.versions)
-      }
-
+      case class CompleteVersionBuilder(parent: VertexBuilder, definitions: Seq[VertexDefinition]) extends VersionBuilder
       case class DefnBuilder(
         parent: VersionBuilder,
         name: Option[String],
         edges: Seq[Edge],
         attributes: Seq[GraphAttribute]
       ) {
-        def edge(edge: Edge): DefnBuilder = this.copy(edges = edge +: edges)
+        def otherEdge(name: String, to: Vertex, attributes: Seq[GraphAttribute] = Seq.empty, toMany: Boolean = false, optional: Boolean = false): DefnBuilder =
+          this.copy(edges = OtherEdge(name, to, toMany, optional, attributes) +: edges)
+
+        def selfEdge(name: String, attributes: Seq[GraphAttribute] = Seq.empty, toMany: Boolean = false, optional: Boolean = false): DefnBuilder =
+          this.copy(edges = SelfEdge(name, attributes, toMany, optional) +: edges)
 
         def attribute(attribute: GraphAttribute): DefnBuilder = this.copy(attributes = attribute +: attributes)
 
-//        def defn(name: Option[String]): DefnBuilder = parent.withDefn(VertexDefinition(name, edges, attributes)).defn(name)
         def defn(name: String): DefnBuilder = parent.withDefn(VertexDefinition(Some(name), edges, attributes)).defn(name)
         def defn: DefnBuilder = parent.withDefn(VertexDefinition(name, edges, attributes)).defn
 
@@ -148,155 +129,7 @@ object Definition {
 
           grandparent.copy(versions = thisVertexVersion +: grandparent.versions).done
         }
-
-        //        def done: CompleteVersionBuilder = parent.withDefn(VertexDefinition(name, edges, attributes))
       }
     }
-
-
-    object Builders2 {
-
-      // Version builder
-
-      case class VertexBuilder1(name: String, versions: Seq[VertexVersion]) {
-        def version(version: VertexVersion): VertexBuilder1 = this.copy(versions = version +: versions)
-
-        def done: Vertex = Vertex(name, versions)
-      }
-
-      def vertex(name: String): VertexBuilder1 = VertexBuilder1(name, Seq.empty)
-
-      // VertexVersion builder
-      case class VertexVersionBuilder1(
-        versions: Seq[VertexDefinition]
-      ) {
-        def defn(version: VertexDefinition): VertexVersionBuilder1 = this.copy(version +: versions)
-
-        def done = VertexVersion(versions)
-      }
-
-//      def vertexVersion(): VertexVersionBuilder1 = VertexVersionBuilder1(Seq.empty)
-
-      // VertexDefn builder
-      case class VertexDefnBuilder2(name: Option[String], edges: Seq[Edge], attr: Seq[GraphAttribute]) {
-        def attr(a: GraphAttribute) = VertexDefnBuilder2(name, edges, a +: attr)
-
-        def end = VertexDefinition(name, edges, attr)
-      }
-
-      case class VertexDefnBuilder1(name: Option[String], edges: Seq[Edge]) {
-        def edge(e: Edge): VertexDefnBuilder1 = this.copy(edges = e +: edges)
-
-        def attr(a: GraphAttribute) = VertexDefnBuilder2(name, edges, Seq.empty)
-      }
-
-      def vertexDefn(name: String): VertexDefnBuilder1 = {
-        VertexDefnBuilder1(Some(name), Seq.empty)
-      }
-
-      def vertexDefn(): VertexDefnBuilder1 = {
-        VertexDefnBuilder1(None, Seq.empty)
-      }
-
-
-
-    }
-
-
-    object Builders {
-      def v(name: String, version: VertexVersion) = Vertex(name, Seq(version))
-
-      def v(name: String, version: Seq[VertexVersion]) = Vertex(name, version)
-
-
-      case class VertexVersionBuilder3(name: Option[String], edges: Seq[Edge], attributes: Seq[GraphAttribute]) {
-        def >>(e: GraphAttribute): VertexVersionBuilder3 = this.copy(attributes = e +: attributes)
-
-        def | : VertexVersion = VertexVersion(VertexDefinition(name, edges, attributes))
-      }
-
-      case class VertexVersionBuilder2(name: Option[String], edges: Seq[Edge]) {
-        def >>(e: Edge): VertexVersionBuilder2 = this.copy(edges = e +: edges)
-
-        def ! : VertexVersionBuilder3 = VertexVersionBuilder3(name, edges, Seq.empty)
-
-        def !(e: GraphAttribute) : VertexVersionBuilder3 = VertexVersionBuilder3(name, edges, Seq(e))
-      }
-
-      class VertexVersionBuilder1() {
-        def >(name: String): VertexVersionBuilder2 = VertexVersionBuilder2(Some(name), Seq.empty)
-        def > : VertexVersionBuilder2 = VertexVersionBuilder2(None, Seq.empty)
-      }
-
-      def vv2(name: String): VertexVersionBuilder2 = VertexVersionBuilder2(Some(name), Seq.empty)
-      def vv2: VertexVersionBuilder2 = VertexVersionBuilder2(None, Seq.empty)
-
-
-      def vv: VertexVersionBuilder1 = new VertexVersionBuilder1()
-
-
-
-      //      def vv: VertexVersionBuilder1 = new VertexVersionBuilder1()
-
-//      def vv(defn: VertexDefinition) = VertexVersion(defn)
-//
-//      def vv(
-//        edges: Seq[Edge],
-//        attributes: Seq[A] = Seq.empty
-//      ) = VertexVersion(VertexDefinition(None, edges, attributes))
-//
-//      def vv(
-//        name: String,
-//        edges: Seq[Edge],
-//        attributes: Seq[A] = Seq.empty
-//      ) = VertexVersion(VertexDefinition(Some(name), edges, attributes))
-//
-//      def vv(attributes: Seq[A]) = VertexVersion(VertexDefinition(None, Seq.empty, attributes))
-//
-//      def vv(name: String, attributes: Seq[A]) = VertexVersion(VertexDefinition(Some(name), Seq.empty, attributes))
-
-//      def vv(
-//        name: String,
-//        edges: Seq[Edge]
-//      ) = VertexVersion(VertexDefinition(Some(name), edges, Seq.empty))
-
-      //
-//      def vv(
-//        edges: Seq[Edge]
-//      ) = VertexVersion(VertexDefinition(None, edges, Seq.empty))
-
-    }
-
-    /*
-          vertex workflowInstance
-          ---
-          definition ArtifactDefn* [
-            foo: String
-            bar: Int
-          ]
-          ---
-          snoo String
-          gloo Int
-          ---
-     */
-
-//    case class BuildingDefinitions(name: String) {
-//      def
-//    }
-
-//    case class EdgeBuilder2()
-//
-//    case class EdgeBuilder1(name: String) {
-//
-//    }
-//
-//    case class BuildingDefinitionsPreSeparator(name: String) {
-//      def ___(name: String): BuildingDefinitions = {
-//        EdgeBuilder1(name)
-//      }
-//    }
-//
-//    def vertex(name: String): BuildingDefinitionsPreSeparator =  BuildingDefinitionsPreSeparator(name)
-
   }
 }
--- src/main/scala/templatey/b/Lake.scala
+++ src/main/scala/templatey/b/Lake.scala
@@ -34,7 +34,9 @@ object Lake {
     {
 
       import graph._
-      import graph.Builders._
+      import graph.Builders3._
+
+      //      import graph.Builders._
 
 
 //      val a: graph.Builders.VertexVersionBuilder1 = vv
@@ -43,230 +45,47 @@ object Lake {
 //      val d: graph.Builders.VertexVersionBuilder3 = c |
 //      val e: graph.VertexVersion = d |
 
-      val e: graph.VertexVersion =
-        (vv > "a" >>
-          OtherEdge("definition", ???)) !
-          Attr.Int("a") |
 
-      val e: graph.VertexVersion =
-        (vv > "a" >>
-          OtherEdge("definition", ???) >>
-          OtherEdge("definition", ???)) !
-          Attr.Int("a") |
-
-      val e: graph.VertexVersion =
-        (vv > "a" >>
-          OtherEdge("definition", ???)) !
-          Attr.Int("a") >>
-          Attr.Int("a") |
-
-//      val a: graph.Builders.VertexVersionBuilder3 =
-//        vv > "a" >>
-//          OtherEdge("definition", ???) |
-//
-//      a.>>()
-
-      //      val vva = vv("atr") >>
-//        OtherEdge("definition", ???) >>
-//        OtherEdge("definition", ???) |
-//        Attr.Int("a") >>
-//        Attr.Int("b")
-
-//      val vva: graph.Builders.VertexVersionBuilder3 =
-//        vv("atr") >>
-//          OtherEdge("definition", ???) >>
-//          OtherEdge("definition", ???) |
-//          (Attr.Int("a")) >>
-//          (Attr.Int("b")) |
-
-//      val vva  =
-//        (vv("atr") >>
-//          OtherEdge("definition", ???) >>
-//          OtherEdge("definition", ???) | Attr.Int("a")).>>(Attr.Int("b")).|
-
-      import graph.Builders2._
-      val x1 =
-        vertexDefn("vd1")
-          .edge(OtherEdge("definition", ???))
-          .edge(OtherEdge("definition", ???))
-          .attr(Attr.Int("a"))
-          .attr(Attr.Int("a"))
-          .end
-
-
-      val vva: graph.VertexVersion =
-        (vv > "atr" >>
-          OtherEdge("definition", ???) >>
-          OtherEdge("definition", ???) |
-          Attr.Int("a")).>>(Attr.Int("b")) |
-
-      val vvb: graph.VertexVersion =
-        (vv2("atr") >>
-          OtherEdge("definition", ???) >>
-          OtherEdge("definition", ???) |
-          Attr.Int("a")).>>(Attr.Int("b")) |
-
-      //        Attr.Int("a") >>
-//        Attr.Int("b")
-
-//      val x = vva.|
-
-      val artifactDefn = v(
-        "ArtifactDefn",
-        vv > "watr" >>
-          OtherEdge("definition", artifactDefn) |
-          Attr.Int("a") |
-
-
-
-        //        vv(
-//          Nil,
-//          Attr.String("label") ::
-//            Nil
-//        )
-      )
+      val artifactDefn =
+        vertex("ArtifactDefn")
+        .version
+        .defn
+        .attribute(Attr.String("label"))
+        .done
 
       val artifact =
         vertex("Artifact")
-        .version(
-          vertexVersion()
-            .defn(
-              vertexDefn("Placeholder")
-                .edge(OtherEdge("definition", artifactDefn))
-                .attr(boolean("exists", false))
-                .end
-            )
-            .defn(
-              vertexDefn("Exists")
-                .edge(OtherEdge("definition", artifactDefn))
-                .attr(boolean("exists", true))
-                .end
-            )
-            .done
-        )
+        .version
+          .defn("Placeholder")
+            .otherEdge("definition", artifactDefn)
+            .attribute(boolean("exists", false))
+          .defn("Exists")
+            .otherEdge("definition", artifactDefn)
+            .attribute(boolean("exists", true))
         .done
 
-//      val artifact =
-//        vertex("Artifact")
-//          .version()
-//              .defn("Placeholder")
-//                  .edge(OtherEdge("definition", artifactDefn))
-//                  .attr(boolean("exists", false))
-//                  .end
-//              .defn("Exists")
-//                  .edge(OtherEdge("definition", artifactDefn))
-//                  .attr(boolean("exists", true))
-//                  .end
-//              .done
-//          )
-//          .done
+      val workflowDefinition =
+        vertex("WorkflowDefn")
+        .version
+        .defn
+        .otherEdge("artifactDefinition", artifactDefn, toMany = true)
+        .attribute(Attr.String("definition"))
+        .done
 
-      {
-        import graph.Builders3._
-
-        val a1: graph.Vertex =
-          vertex("v")
-            .version
-              .defn("d1")
-                .edge(OtherEdge("definition", artifactDefn))
-                .attribute(boolean("exists", false))
-              .defn("d2")
-                .edge(OtherEdge("definition", artifactDefn))
-                .attribute(boolean("exists", false))
-            .version
-              .defn("d1")
-                .edge(OtherEdge("definition", artifactDefn))
-                .attribute(boolean("exists", false))
-              .defn
-                .edge(OtherEdge("definition", artifactDefn))
-                .attribute(boolean("exists", false))
-          .done
-
-
-      }
-
-      val artifact = v(
-        "Artifact",
-        vv(
-          "Placeholder",
-          OtherEdge("definition", artifactDefn) :: Nil,
-          boolean("exists", false) :: Nil // need to create early, to keep track of its uid; this will be pattern.
-        ) ::
-        vv(
-          "Exists",
-          OtherEdge("definition", artifactDefn) :: Nil,
-          boolean("exists", false) :: Nil // need to create early, to keep track of its uid; this will be pattern.
-        ) :: Nil
-      )
-
-      val workflowDefinition = Vertex(
-        "WorkflowDefn",
-        vv(
-          OtherEdge("artifactDefinition", artifactDefn, toMany = true) :: Nil,
-            Attr.String("definition") :: Nil
-        )
-      )
-
-      /*
-        val workflowInstance =
-          v("name")
-            e("output",  artifact)+ : ToMany ::
-            e("input",  artifact) : ToMany ::
-
-
-          vertex: workflowInstance
-          ---
-          -[definition]-> artifactDefn *
-             foo: String
-             bar: Int
-          -[blah]-> thing `1`
-          ---
-          snoo: String
-          gloo: Int
-          ---
-
-
-          vertex: workflowInstance
-          ---
-          definition: ArtifactDefn* [
-            foo: String
-            bar: Int
-          ]
-          ---
-          snoo: String
-          gloo: Int
-          ---
-       */
-
-
-
-
-      val workflowInstance = Vertex(
-        "WorkflowInstance",
-        vv(
-          "Running",
-
-          OtherEdge("output", artifact, toMany = true) ::
-          OtherEdge("definition", workflowDefinition) ::
-            Nil,
-
-          Attr.String("jobUid") ::
-          Attr.String("status", Some("Running")) ::
-            Nil
-        ) ::
-        vv(
-          "Complete",
-
-          OtherEdge("output", artifact, toMany = true) ::
-            OtherEdge("definition", workflowDefinition) ::
-            Nil,
-
-          Attr.String("jobUid") ::
-            Attr.String("status") ::
-            Nil
-        ) :: Nil
-
-      )
+      val workflowInstance =
+        vertex("WorkflowInstance")
+        .version
+          .defn("Running")
+            .otherEdge("output", artifact, toMany = true)
+            .otherEdge("definition", workflowDefinition)
+            .attribute(Attr.String("jobUid"))
+            .attribute(Attr.String("status", Some("Running")))
+          .defn("Complete")
+            .otherEdge("output", artifact, toMany = true)
+            .otherEdge("definition", workflowDefinition)
+            .attribute(Attr.String("jobUid"))
+            .attribute(Attr.String("status"))
+        .done
 
       val graphDefinition = GraphLibraryFactory.write(
         artifactDefn :: artifact :: workflowDefinition :: workflowInstance :: Nil
