efab924e7c498c3569ee8312d75ead0f3b3971e4
Max Wittmann <damxam@gmail.com>
2018-05-13 20:13:47 +1000

    Clean more

--- src/main/scala/io/grafgraph/render/crud/RenderState.scala
+++ src/main/scala/io/grafgraph/render/crud/RenderState.scala
@@ -35,31 +35,31 @@ object RenderState {
        |
        |) $extendsPart
        |
-       |${renderStateCreate(vertex, state)}
+       |${renderCreateMethod(vertex, state)}
        """.stripMargin
   }
 
 
-  private def renderStateCreate(vertex: WithBuilders#Vertex, state: WithBuilders#VertexState): String = {
-    val renderParams = genRenderParams(vertex, state)
+  private def renderCreateMethod(vertex: WithBuilders#Vertex, state: WithBuilders#VertexState): String = {
+    val renderParams = renderParams(vertex, state)
 
     val query =
       s"""
-         ||CREATE (${state.name}: Class_${state.name} {${renderCreateAttributes(vertex, state)}})
+         ||CREATE (${state.name}: Class_${state.name} {${renderCypherAttributes(vertex, state)}})
        """.stripMargin
 
     // Todo: Return object
     //def create(ele: ${state.name}): ${state.name} = {
     s"""
        |def create(ele: ${state.name}): Unit = {
-       |  ${neo4jTx(query, renderParams)}
+       |  ${renderNeo4jTx(query, renderParams)}
        |}
        """.stripMargin
   }
 
-  private def genRenderParams(vertex: WithBuilders#Vertex, state: WithBuilders#VertexState): String = {
+  private def renderParams(vertex: WithBuilders#Vertex, state: WithBuilders#VertexState): String = {
     val params = state.allAttributes.map { (attr: Attribute) =>
-      s""""${attr.name}" -> ${genRenderParam(vertex, state, attr)}"""
+      s""""${attr.name}" -> ${renderParam(vertex, state, attr)}"""
     }.mkString(",")
 
     s"""
@@ -69,27 +69,27 @@ object RenderState {
        """.stripMargin
   }
 
-  def genRenderParam(vertex: WithBuilders#Vertex, state: WithBuilders#VertexState, attr: Attribute): String = attr match {
+  private def renderParam(vertex: WithBuilders#Vertex, state: WithBuilders#VertexState, attr: Attribute): String = attr match {
     case Attr.UID(name) => s"ele.$name.toString"
     case Attr.String(name, value) => value.getOrElse(s"ele.$name")
     case Attr.Int(name, value) => value.map(_.toString).getOrElse(s"ele.$name")
     case Attr.Boolean(name, value) => value.map(b => s"$b.booleanValue().asInstanceOf[Object]").getOrElse(s"ele.$name")
   }
 
-  private def renderCreateAttributes(vertex: WithBuilders#Vertex, state: WithBuilders#VertexState): String = {
-    state.allAttributes.map(renderCreateAttribute).mkString(", ")
+  private def renderCypherAttributes(vertex: WithBuilders#Vertex, state: WithBuilders#VertexState): String = {
+    state.allAttributes.map(renderCypherAttribute).mkString(", ")
   }
 
-  private def renderCreateAttribute(attr: Attribute): String = s"${attr.name}: {${attr.name}}"
+  private def renderCypherAttribute(attr: Attribute): String = s"${attr.name}: {${attr.name}}"
 
-  private def neo4jTx(query: String, params: String): String =
+  private def renderNeo4jTx(query: String, params: String): String =
     s"""
        |val session = graph.driver.session()
        |
-         |session.writeTransaction { tx =>
+       |session.writeTransaction { tx =>
        |  val params = $params
        |
-         |  tx.run(\"\"\"$query\"\"\".stripMargin, params)
+       |  tx.run(\"\"\"$query\"\"\".stripMargin, params)
        |  tx.success()
        |}
        |session.close()
