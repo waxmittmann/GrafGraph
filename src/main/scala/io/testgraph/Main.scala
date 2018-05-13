2e56ace8a7bf2ea38a88b6460fdfa0f6ab630d55
Max Wittmann <damxam@gmail.com>
2018-05-13 20:14:02 +1000

    This one too

--- src/main/scala/io/testgraph/Main.scala
+++ src/main/scala/io/testgraph/Main.scala
@@ -1,23 +1,23 @@
-//package io.testgraph
-//
-//import java.util.UUID
-//
-//object Main {
-//
-//  def main(args: Array[String]): Unit = {
-//
-//    val graphClient = new Neo4jGraph("bolt://127.0.0.1:7687", "neo4j", "test")
-//
-//    val graph = new Graph(graphClient)
-//
-//    val artifactDefn = graph.ArtifactDefn.Instance(
-//      UUID.randomUUID(),
-//      "labelA"
-//    )
-//
-//    graph.ArtifactDefn.create(artifactDefn)
-//
-//    graphClient.close()
-//  }
-//
-//}
+package io.testgraph
+
+import java.util.UUID
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+    val graphClient = new Neo4jGraph("bolt://127.0.0.1:7687", "neo4j", "test")
+
+    val graph = new Graph(graphClient)
+
+    val artifactDefn = graph.ArtifactDefn.Instance(
+      UUID.randomUUID(),
+      "labelA"
+    )
+
+    graph.ArtifactDefn.create(artifactDefn)
+
+    graphClient.close()
+  }
+
+}
